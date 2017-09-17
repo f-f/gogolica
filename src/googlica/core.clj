5379eaf3e2ca486a22e895ffc6719b4a4887087d
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2017-09-17 21:58:33 +0300

    M-x whitespace-cleanup

--- src/googlica/core.clj
+++ src/googlica/core.clj
@@ -15,18 +15,18 @@
 
 (def model-paths
   (->> (fs/find-files "model" #".*\.json")
-       (mapv #(.getPath %)))) 
+       (mapv #(.getPath %))))
 
 (defn read-model [path]
   (-> path
       slurp
-      (json/decode true))) 
+      (json/decode true)))
 
-(def model-maps (mapv read-model model-paths)) 
+(def model-maps (mapv read-model model-paths))
 
 (def storage-model (first (filter #(= (:id %) "storage:v1") model-maps)))
 
-(def storage-object-get (-> storage-model :resources :objects :methods :get)) 
+(def storage-object-get (-> storage-model :resources :objects :methods :get))
 
 (def base-url (-> storage-model :baseUrl))
 
@@ -46,14 +46,14 @@
   [method]
   (->> (:parameters method)
        ((juxt filter remove) (fn [[k v]] (:required v)))
-       (mapv #(into {} %)))) 
+       (mapv #(into {} %))))
 
 (defn generate-function-name
   "Generates a symbol in the form of 'verb-resource', from a method map."
   [method]
   (let [[_ resource-name method-name] (s/split (:id method) #"\.")]
     (-> (str method-name "-" resource-name)
-        ->kebab-case-symbol))) 
+        ->kebab-case-symbol)))
 
 (defn generate-docs [method]
   (str (:description method)
@@ -63,7 +63,7 @@
   (let [[required optional] (->> method
                                  split-required-params
                                  (mapv (comp (partial mapv ->kebab-case-symbol) keys)))]
-    `[~@required {:keys ~optional}])) 
+    `[~@required {:keys ~optional}]))
 
 ;; > ( template->path-vector "b/{fooBar}/o/{bar}" ["fooBar" "bar"])
 ;; => ["/b/" foo-bar "/c/" bar ]
@@ -101,7 +101,7 @@
 
 (defn generate-request [base-url method]
   {:method (-> method :httpMethod s/lower-case keyword)
-   :url `(str ~base-url ~@(generate-path method))}) 
+   :url `(str ~base-url ~@(generate-path method))})
 
 (defn generate-function-from-method
   [base-url method]

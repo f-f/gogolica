051d94b8b58b78c4679a7ca4b276c1a5d98661fe
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2017-09-26 14:57:50 +0300

    Add clojure caching library

--- project.clj
+++ project.clj
@@ -4,17 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.cache "0.6.5"]
                  [cheshire "5.8.0"]
                  [me.raynes/fs "1.4.6"]
                  [clj-http "3.7.0"]
                  [fipp "0.6.10"]
                  [buddy/buddy-sign "2.2.0"]
-                 [clj-jwt "0.1.1"]
-                 [base64-clj "0.1.1"]
-                 [buddy/buddy-core "1.4.0"] 
+                 [buddy/buddy-core "1.4.0"]
                  [clj-time "0.14.0"]
-                 [com.taoensso/nippy "2.11.1"]
-                 [cheshire "5.8.0"]
                  [camel-snake-kebab "0.4.0"]]
   :main ^:skip-aot googlica.core
   :target-path "target/%s"

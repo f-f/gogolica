1263545e4ee2c1888adcf2e45f91404db1724fbe
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2017-10-10 21:31:02 +0300

    Update dependencies to get clojure.spec and tools

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-beta2"]
+                 [metosin/spec-tools "0.3.3"]
                  [org.clojure/core.cache "0.6.5"]
                  [cheshire "5.8.0"]
                  [me.raynes/fs "1.4.6"]
